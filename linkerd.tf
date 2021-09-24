11e4abc43009c615a705baded6d314ef8d0795a1
Eric Rumble <ericwrumble@gmail.com>
2021-09-24 19:13:34 -0400

    add traefik to the linkerd service mesh

--- linkerd.tf
+++ linkerd.tf
@@ -11,3 +11,36 @@ module "linkerd-viz" {
     module.linkerd
   ]
 }
+
+# Add Traefik to the Linkerd service mesh
+resource "kubernetes_manifest" "traefik_pod_annotations" {
+  manifest = {
+    apiVersion = "helm.cattle.io/v1"
+    kind = "HelmChartConfig"
+
+    metadata = {
+      name = "traefik"
+      namespace = "kube-system"
+
+      labels = {
+        "app.kubernetes.io/name"       = "traefik-linkerd-mesh"
+        "app.kubernetes.io/instance"   = "traefik-linkerd-mesh"
+        "app.kubernetes.io/managed-by" = "Terraform"
+      }
+    }
+
+    spec = {
+      valuesContent = yamlencode({
+        deployment = {
+          podAnnotations = {
+            "linkerd.io/inject" = "enabled"
+          }
+        }
+      })
+    }
+  }
+
+  depends_on = [
+    module.linkerd
+  ]
+}
