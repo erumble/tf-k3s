0e6248c388329665b5030bb5a536e30b516efffc
Eric Rumble <ericwrumble@gmail.com>
2021-09-24 20:01:01 -0400

    expose linkerd-viz dashboard

--- linkerd.tf
+++ linkerd.tf
@@ -16,10 +16,10 @@ module "linkerd-viz" {
 resource "kubernetes_manifest" "traefik_pod_annotations" {
   manifest = {
     apiVersion = "helm.cattle.io/v1"
-    kind = "HelmChartConfig"
+    kind       = "HelmChartConfig"
 
     metadata = {
-      name = "traefik"
+      name      = "traefik"
       namespace = "kube-system"
 
       labels = {
@@ -43,4 +43,72 @@ resource "kubernetes_manifest" "traefik_pod_annotations" {
   depends_on = [
     module.linkerd
   ]
+}
+
+# Expose Linkerd dashboard
+resource "kubernetes_manifest" "linkerd_external_dashboard" {
+  manifest = {
+    apiVersion = "traefik.containo.us/v1alpha1"
+    kind       = "IngressRoute"
+
+    metadata = {
+      name      = "linkerd-external-dashboard"
+      namespace = module.linkerd-viz.namespace
+
+      labels = {
+        "app.kubernetes.io/name"       = "linkerd-external-dashboard"
+        "app.kubernetes.io/instance"   = "linkerd-external-dashboard"
+        "app.kubernetes.io/managed-by" = "Terraform"
+      }
+    }
+
+    spec = {
+      entryPoints = ["web"]
+      routes = [{
+        kind  = "Rule"
+        match = "Host(`linkerd.lvh.me`)"
+
+        services = [{
+          kind = "Service"
+          name = "web"
+          port = 8084
+        }]
+
+        middlewares = [{
+          name      = kubernetes_manifest.linkerd_external_dashboard_headers.manifest.metadata.name
+          namespace = kubernetes_manifest.linkerd_external_dashboard_headers.manifest.metadata.namespace
+        }]
+      }]
+    }
+  }
+
+  depends_on = [
+    kubernetes_manifest.linkerd_external_dashboard_headers
+  ]
+}
+
+resource "kubernetes_manifest" "linkerd_external_dashboard_headers" {
+  manifest = {
+    apiVersion = "traefik.containo.us/v1alpha1"
+    kind       = "Middleware"
+
+    metadata = {
+      name      = "linkerd-external-dashboard-headers"
+      namespace = module.linkerd-viz.namespace
+
+      labels = {
+        "app.kubernetes.io/name"       = "linkerd-external-dashboard-headers"
+        "app.kubernetes.io/instance"   = "linkerd-external-dashboard-headers"
+        "app.kubernetes.io/managed-by" = "Terraform"
+      }
+    }
+
+    spec = {
+      headers = {
+        customRequestHeaders = {
+          Host = "web.linkerd-viz.svc"
+        }
+      }
+    }
+  }
 }
