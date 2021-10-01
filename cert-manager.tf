eeb86473054352f018487b892319a04491ae7e53
Eric Rumble <ericwrumble@gmail.com>
2021-10-01 03:28:50 -0400

    create self signed cluster issuer, and ca issuer

--- cert-manager.tf
+++ cert-manager.tf
@@ -1,3 +1,24 @@
 module "cert_manager" {
   source = "./modules/cert-manager"
 }
+
+module "self_signed_cluster_issuer" {
+  source = "./modules/self-signed-cluster-issuer"
+
+  namespace = module.cert_manager.namespace
+
+  depends_on = [
+    module.cert_manager,
+  ]
+}
+
+module "kube_system_ca_issuer" {
+  source = "./modules/ca-issuer"
+
+  cluster_issuer_name = module.self_signed_cluster_issuer.name
+  namespace           = "kube-system"
+
+  depends_on = [
+    module.self_signed_cluster_issuer,
+  ]
+}
